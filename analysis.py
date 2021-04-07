8c783891cad6dc36b63bc24e5551c9c868e08ed8
Izardo <isabelladoylegti@gmail.com>
2021-04-07 15:19:08 +0100

    added code to get individual summaries of data

--- analysis.py
+++ analysis.py
@@ -15,16 +15,34 @@ def dataInfo(x):
     x = df.info()
     print(x)
 
-# prints full dataset
+# prints full dataset to compare against Fisher's 
 def viewFull(x):
     pd.set_option('display.max_rows', 150)
     print(x)
 
 # view summary shows basic statistical information
-def viewSummary(x):
+# may not be needed but gives us an instant range of values to familariase ourselves with the data
+def viewFullSummary(x):
     describe = x.describe()
     print(describe)
-   
+
+# side-by-side summary of species
+# REF: Describe Function With Groupby Pandas (Python 3.5.1), 7 Apr. 2021, ayhan,
+# https://stackoverflow.com/questions/40346436/describe-function-with-groupby-pandas-python-3-5-1
+def distinctSummary(x):
+    summary = x.groupby('species').describe()
+    summary.round(decimals = 2)
+    print(summary)
+
+# seperate summaries
+def individualSummary(x, y):    # x = the dataframe you wish to use, y = the column you wish to get a summary of
+    summary = x[x["species"] == y].describe()   # Ref: "Lesson 3. Run Calculations and Summary Statistics on Pandas Dataframes"
+                                                # Jenny Palomino, Leah Wasser, 7 Apr. 2021, www.earthdatascience.org/courses/intro-to-earth-data-science/scientific-data-structures-python/pandas-dataframes/run-calculations-summary-statistics-pandas-dataframes/
+    print(summary)
+
+individualSummary(df, "Iris-setosa")
+#distinctSummary(df)
+#viewFull(df)  
 #dataInfo(df)
 #viewSummary(df)
 
