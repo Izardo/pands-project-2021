43a07d0638c47d9b4c612dcef6cb1d43e2f74e37
Izardo <isabelladoylegti@gmail.com>
2021-04-28 16:12:44 +0100

    added code in hist() function to move legend&title

--- analysis.py
+++ analysis.py
@@ -82,24 +82,28 @@ speciesList = [iris_setosa, iris_versicolor, iris_virginica]
 # This function plots a histogram & density plot for each attribute
 def hist():
     # plots sepal length values
-    sns.FacetGrid(df,hue="species",height=6).map(sns.distplot,"sepal_length").add_legend(fontsize=12)   # increased legend fontsize # hue inclued each subset (species) of data in the plot
-    plt.title("Sepal Length Histogram & Density Plot", pad=20)
+    sns.FacetGrid(df,hue="species", height=6, legend_out=False).map(sns.distplot,"sepal_length").add_legend(fontsize=10)   # increased legend fontsize # hue inclued each subset (species) of data in the plot
+    plt.title("Sepal Length Histogram & Density Plot")
     plt.xlabel("Sepal Length in cm")
+    plt.subplots_adjust(top=0.8)
     plt.savefig('pngs/distinctHist_sepal_length') # saves pngs of plots in pngs folder
     # plots sepal width values
-    sns.FacetGrid(df,hue="species",height=6).map(sns.distplot,"sepal_width").add_legend(fontsize=12)
-    plt.title("Sepal Width Histogram & Density Plot", pad=20)
+    sns.FacetGrid(df,hue="species", height=6, legend_out=False).map(sns.distplot,"sepal_width").add_legend(fontsize=10)
+    plt.title("Sepal Width Histogram & Density Plot")
     plt.xlabel("Sepal width in cm")
+    plt.subplots_adjust(top=0.8)
     plt.savefig('pngs/distinctHist_sepal_width')
     # plots petal length values
-    sns.FacetGrid(df,hue="species",height=6).map(sns.distplot,"petal_length").add_legend(fontsize=12)
-    plt.title("Petal Length Histogram & Density Plot", pad=20)
+    sns.FacetGrid(df,hue="species", height=6, legend_out=False).map(sns.distplot,"petal_length").add_legend(fontsize=10)
+    plt.title("Petal Length Histogram & Density Plot")
     plt.xlabel("Petal length in cm")
+    plt.subplots_adjust(top=0.8)
     plt.savefig('pngs/distinctHist_petal_length')
     # plots petal width values
-    sns.FacetGrid(df,hue="species",height=6).map(sns.distplot,"petal_width").add_legend(fontsize=12)
-    plt.title("Petal Width Histogram & Density Plot", pad=20)
+    sns.FacetGrid(df,hue="species", height=6, legend_out=False).map(sns.distplot,"petal_width").add_legend(fontsize=10)
+    plt.title("Petal Width Histogram & Density Plot")
     plt.xlabel("Petal width in cm")
+    plt.subplots_adjust(top=0.8)
     plt.savefig('pngs/distinctHist_petal_width')
     # matplotlib opens 4 seperate windows displaying the plots just created
     plt.show()
